ff34045f692e4b55536a183b8ad7adfbb9379592
pablo <remagforce@gmail.com>
2020-10-30 15:51:33 -0600

    agregacion de fecha y hora a ingresos

--- app/src/main/java/com/progra/projecto_cartera/Ingresos.java
+++ app/src/main/java/com/progra/projecto_cartera/Ingresos.java
@@ -2,22 +2,39 @@ package com.progra.projecto_cartera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
-public class Ingresos extends AppCompatActivity {
+import java.util.Calendar;
 
-    public EditText edt_n1,edt_txt1;
+public class Ingresos extends AppCompatActivity implements View.OnClickListener {
+
+    public EditText edt_n1,edt_txt1, edt_fecha, edt_hora;
     public Spinner spinner1;
+    private int dia,mes,año,hora,minutos;
+    Button bfecha,bhora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresos);
+
+        //comunicacion con los edt y botones  de fecha y hora
+        edt_fecha = (EditText)findViewById(R.id.edt_fecha);
+        edt_hora = (EditText)findViewById(R.id.edt_hora);
+        bfecha =(Button)findViewById(R.id.btnfecha);
+        bhora = (Button)findViewById(R.id.btnhora);
+        bfecha.setOnClickListener(this);
+        bhora.setOnClickListener(this);
 
         //comunicacion con el spinner
         spinner1 = (Spinner)findViewById(R.id.spinazo);
@@ -30,18 +47,40 @@ public class Ingresos extends AppCompatActivity {
 
 
 
+    //metodo para los botones de fecha y hora
+    @Override
+    public void onClick(View v) {
+        if(v == bfecha){
+            final Calendar c= Calendar.getInstance();
+            dia = c.get(Calendar.DAY_OF_MONTH);
+            mes = c.get(Calendar.MONTH);
+            año = c.get(Calendar.YEAR);
 
-   //metodo para el boton de guardar fecha
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    edt_fecha.setText(dayOfMonth + "/" + (month+1) + "/" + year);
+                }
+            }
+                    ,dia,mes,año);
+            datePickerDialog.show();
+        }
+        if(v == bhora){
+            final Calendar c= Calendar.getInstance();
+            hora = c.get(Calendar.HOUR_OF_DAY);
+            minutos = c.get(Calendar.MINUTE);
 
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    edt_hora.setText(hourOfDay + ":" + minute);
+                }
+            }
+            ,hora,minutos,false);
+            timePickerDialog.show();
+        }
 
-
-
-
-
-
-
-
-
+    }
 
 
 
@@ -62,5 +101,6 @@ public class Ingresos extends AppCompatActivity {
         Intent movimiento = new Intent(this,Movimientos.class);
         startActivity(movimiento);
     }
+
 
 }
