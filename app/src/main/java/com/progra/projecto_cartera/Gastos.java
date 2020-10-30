14a6888824d88806276fd720f1254835eaa74176
pablo <remagforce@gmail.com>
2020-10-29 19:25:31 -0600

    agregacion de botones a los activitys

--- app/src/main/java/com/progra/projecto_cartera/Gastos.java
+++ app/src/main/java/com/progra/projecto_cartera/Gastos.java
@@ -2,7 +2,9 @@ package com.progra.projecto_cartera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Gastos extends AppCompatActivity {
 
@@ -11,4 +13,37 @@ public class Gastos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gastos);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //metodos de los botones para navegar entre activitys
+    public void Inicio (View view){
+        Intent inicio = new Intent(this, MainActivity.class);
+        startActivity(inicio);
+    }
+
+    public void Ingreso (View view){
+        Intent ingreso = new Intent(this, Ingresos.class);
+        startActivity(ingreso);
+    }
+
+    public void Movimientos (View view){
+        Intent movimiento = new Intent(this,Movimientos.class);
+        startActivity(movimiento);
+    }
+
+
 }
--- app/src/main/java/com/progra/projecto_cartera/Ingresos.java
+++ app/src/main/java/com/progra/projecto_cartera/Ingresos.java
@@ -2,7 +2,9 @@ package com.progra.projecto_cartera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Ingresos extends AppCompatActivity {
 
@@ -11,4 +13,23 @@ public class Ingresos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresos);
     }
+
+
+
+    //metodos de los botones para navegar entre activitys
+    public void Inicio (View view){
+        Intent inicio = new Intent(this, MainActivity.class);
+        startActivity(inicio);
+    }
+
+    public void Gasto (View view){
+        Intent gasto = new Intent(this,Gastos.class);
+        startActivity(gasto);
+    }
+
+    public void Movimientos (View view){
+        Intent movimiento = new Intent(this,Movimientos.class);
+        startActivity(movimiento);
+    }
+
 }
