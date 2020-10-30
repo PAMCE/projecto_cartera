fdc3df285e9bc50c529ca5e8245183aae1cdc6da
pablo <remagforce@gmail.com>
2020-10-29 19:41:31 -0600

    cambios no muy importantes

--- app/src/main/java/com/progra/projecto_cartera/Movimientos.java
+++ app/src/main/java/com/progra/projecto_cartera/Movimientos.java
@@ -2,7 +2,9 @@ package com.progra.projecto_cartera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Movimientos extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class Movimientos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movimientos);
+    }
+
+
+
+    //metodos de los botones para navegar entre activitys
+    public void Inicio (View view){
+        Intent inicio = new Intent(this, MainActivity.class);
+        startActivity(inicio);
+    }
+
+    public void Ingreso (View view){
+        Intent ingreso = new Intent(this, Ingresos.class);
+        startActivity(ingreso);
+    }
+
+    public void Gasto (View view){
+        Intent gasto = new Intent(this,Gastos.class);
+        startActivity(gasto);
     }
 }
